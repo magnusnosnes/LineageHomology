74d7a4106cdc99e77b11a3d3db16d400ec793f58
magnusnosnes <magnusnosnes@gmail.com>
2021-04-13 22:02:00 +0200

    Adding some useful functions.

--- R/Import_export_ace_output.R
+++ R/Import_export_ace_output.R
@@ -20,7 +20,7 @@ library(ips)
 # consensus_Q = summary(mapped_Q)
 # plot(consensus_Q)
 # nodelabels()
-# 
+#
 # fit1 = ace(x=loc, phy= tree_test, type="discrete", mod="ARD")
 # tree_test$tip.label
 # fit1
@@ -43,15 +43,31 @@ library(ips)
 
 #For testing
 
+#' Title
+#'
+#' @param tree #Phylogenetic tree
+#' @param ace_nodes #Node probabilities in ace format
+#' @param ace_tips #Tip probabilities in ace format
+#' @param give_tips #Taxa to do the computations for
+#' @param start_time Date of root in the phylogeny.
+#'
+#' @return
+#' @export
+#'
+#' @importFrom phytools nodeHeights nodeheight
+#' @importFrom ips descendants
+#' @importFrom ape nodepath
+#' @importFrom ape nodepath
+#' @examples
 count_import_export_ace = function(tree,ace_nodes,ace_tips, give_tips=NA,start_time=NA) {
-  
+
   #The node number of the tips are
   tip_nodes = sapply(tree$tip.label,function(x,y) which(y==x),y=tree$tip.label)
-  
+
   if(is.na(give_tips)==F){ #If the count is among given tips.
     tip_nodes = sapply(give_tips,function(x,y) which(y==x),y=tree$tip.label)
   }
-  
+
   tip_states = ace_tips
   node_states = ace_nodes
   rownames(node_states)=(length(tree$tip.label)+1):(2*length(tree$tip.label)-1)
@@ -59,8 +75,8 @@ count_import_export_ace = function(tree,ace_nodes,ace_tips, give_tips=NA,start_t
   #Parent and child.
   parent <- tree$edge[, 1]
   child <- tree$edge[, 2]
-  
-  
+
+
   #Size of lineage
   lineage_size = c()
   #Tips in lineage
@@ -69,16 +85,16 @@ count_import_export_ace = function(tree,ace_nodes,ace_tips, give_tips=NA,start_t
   mrca.list = c()
   #Initialize importations,
   importations = 0
-  
+
   #Initialize
   local_transmissions = 0
   #Initialize index counter
   counter=1
-  
-  
-  
+
+
+
   checked_most_basal = F
-  
+
   while(length(tip_nodes)>0) {
     current_node = tip_nodes[1]
     tip_label_list = c(names(current_node)) #Set up a list containing tips for the current tip.
@@ -89,38 +105,38 @@ count_import_export_ace = function(tree,ace_nodes,ace_tips, give_tips=NA,start_t
     ancestor_node = parent[which(child==current_node)]
     ancestor_node_row = which(rownames(node_states)==ancestor_node)
     ancestor_state =which.max(node_states[ancestor_node_row,])
-    
+
     if(current_state!=ancestor_state) {  #If more than one mapping on the branch, then it is estimated as an importation.
       tips_in_segment = 1 #Count how many tips are considered for each mapping segment.
     }
-    
+
     else {
-      while(current_state==ancestor_state) { #While there is no transition, move one branch up. 
-        
+      while(current_state==ancestor_state) { #While there is no transition, move one branch up.
+
         if(((current_node%in%child)==T)) {
           current_node = ancestor_node #If the loop gets all the way through, update ancestor node.
           ancestor_node = parent[which(child==current_node)]
-          if((current_node%in%child)==F) { #break loop if we're already at the root node. 
+          if((current_node%in%child)==F) { #break loop if we're already at the root node.
             goes_all_the_way_back = T
             break()
           }
           #Note that current state doesn't need to be updated since we're moving up if it is the same.
           ancestor_node_row = which(rownames(node_states)==ancestor_node)
-          ancestor_state = which.max(node_states[ancestor_node_row,])       
+          ancestor_state = which.max(node_states[ancestor_node_row,])
         }
-        
+
       }
       tip_search_subspace = descendants(tree, current_node)
       subspace=tip_search_subspace[tip_search_subspace%in%tip_nodes] #Find all possible tips descending from the current node.
-      
-      if(length(subspace)!=0) { 
+
+      if(length(subspace)!=0) {
         for(i in 1:length(subspace)) { #Check for transitions on nodepath to each tip.
           nodes_to_check = nodepath(tree, from = current_node, to=subspace[i])[-1] #path to the tips
           internal = nodes_to_check[-length(nodes_to_check)]
           nods= sapply(internal, FUN = function(x) which.max(node_states[which(rownames(node_states)==x),])) #node states
           tps = which.max(tip_states[nodes_to_check[length(nodes_to_check)],]) #Tip state
           both = c(nods, tps)
-          
+
           if(sum(both==current_state)==length(both)) { #If all states are equal to the state of the current node proceed.
             tips_in_segment = tips_in_segment+1
             tip_label_list = c(tip_label_list, names(tip_nodes[which(tip_nodes==nodes_to_check[length(nodes_to_check)])])) #Add the tips that will be removed.
@@ -129,7 +145,7 @@ count_import_export_ace = function(tree,ace_nodes,ace_tips, give_tips=NA,start_t
         }
       }
     }
-    
+
     if(tips_in_segment > 1 ) {
       local_transmissions = local_transmissions+(tips_in_segment-1)
       importations = importations+1
@@ -138,7 +154,7 @@ count_import_export_ace = function(tree,ace_nodes,ace_tips, give_tips=NA,start_t
       importations = importations+1
     }
     lineage_size = c(lineage_size,tips_in_segment)
-    
+
     lineage_tips[[counter]]=tip_label_list
     mrca.list[counter] = current_node
     counter = counter+1 #move one up to fill the next index on in the next loop iteration.
@@ -151,19 +167,31 @@ count_import_export_ace = function(tree,ace_nodes,ace_tips, give_tips=NA,start_t
   if(start_time){
     lineage_mrca =lineage_mrca+start_time
   }
-  return(list("Import_Export"=c(importations, local_transmissions),"Lineage_sizes"=lineage_size,"Taxa_names"=lineage_tips, "MRCA's"=lineage_mrca))  
-  
+  return(list("Import_Export"=c(importations, local_transmissions),"Lineage_sizes"=lineage_size,"Taxa_names"=lineage_tips, "MRCA's"=lineage_mrca))
+
 }
 
+#' Title
+#'
+#' @param tree #Phylogenetic tree
+#' @param ace_nodes #Node probabilities in ace format
+#' @param ace_tips #Tip probabilities in ace format
+#' @param give_tips #Taxa to do the computations for
+#' @param start_time Date of root in the phylogeny.
+#'
+#' @return
+#' @export
+#'
+#' @examples
 count_import_export_ace_uncertainty = function(tree,ace_nodes,ace_tips, give_tips=NA,start_time=NA) {
-  
+
   #The node number of the tips are
   tip_nodes = sapply(tree$tip.label,function(x,y) which(y==x),y=tree$tip.label)
-  
+
   if(is.na(give_tips)==F){ #If the count is among given tips.
     tip_nodes = sapply(give_tips,function(x,y) which(y==x),y=tree$tip.label)
   }
-  
+
   tip_states = ace_tips
   node_states = ace_nodes
   rownames(node_states)=(length(tree$tip.label)+1):(2*length(tree$tip.label)-1)
@@ -171,8 +199,8 @@ count_import_export_ace_uncertainty = function(tree,ace_nodes,ace_tips, give_tip
   #Parent and child.
   parent <- tree$edge[, 1]
   child <- tree$edge[, 2]
-  
-  
+
+
   #Size of lineage
   lineage_size = c()
   #Tips in lineage
@@ -181,16 +209,16 @@ count_import_export_ace_uncertainty = function(tree,ace_nodes,ace_tips, give_tip
   mrca.list = c()
   #Initialize importations,
   importations = 0
-  
+
   #Initialize
   local_transmissions = 0
   #Initialize index counter
   counter=1
-  
-  
-  
+
+
+
   checked_most_basal = F
-  
+
   while(length(tip_nodes)>0) {
     current_node = tip_nodes[1]
     #print(current_node)
@@ -202,38 +230,38 @@ count_import_export_ace_uncertainty = function(tree,ace_nodes,ace_tips, give_tip
     ancestor_node = parent[which(child==current_node)]
     ancestor_node_row = which(rownames(node_states)==ancestor_node)
     ancestor_state =sample(1:2,1,prob=node_states[ancestor_node_row,])
-    
+
     if(current_state!=ancestor_state) {  #If more than one mapping on the branch, then it is estimated as an importation.
       tips_in_segment = 1 #Count how many tips are considered for each mapping segment.
     }
-    
+
     else {
-      while(current_state==ancestor_state) { #While there is no transition, move one branch up. 
-        
+      while(current_state==ancestor_state) { #While there is no transition, move one branch up.
+
         if(((current_node%in%child)==T)) {
           current_node = ancestor_node #If the loop gets all the way through, update ancestor node.
           ancestor_node = parent[which(child==current_node)]
-          if((current_node%in%child)==F) { #break loop if we're already at the root node. 
+          if((current_node%in%child)==F) { #break loop if we're already at the root node.
             goes_all_the_way_back = T
             break()
           }
           #Note that current state doesn't need to be updated since we're moving up if it is the same.
           ancestor_node_row = which(rownames(node_states)==ancestor_node)
-          ancestor_state = sample(1:2,1,prob=node_states[ancestor_node_row,])       
+          ancestor_state = sample(1:2,1,prob=node_states[ancestor_node_row,])
         }
-        
+
       }
       tip_search_subspace = descendants(tree, current_node)
       subspace=tip_search_subspace[tip_search_subspace%in%tip_nodes] #Find all possible tips descending from the current node.
-      
-      if(length(subspace)!=0) { 
+
+      if(length(subspace)!=0) {
         for(i in 1:length(subspace)) { #Check for transitions on nodepath to each tip.
           nodes_to_check = nodepath(tree, from = current_node, to=subspace[i])[-1] #path to the tips
           internal = nodes_to_check[-length(nodes_to_check)]
           nods= sapply(internal, FUN = function(x) which.max(node_states[which(rownames(node_states)==x),])) #node states
           tps = which.max(tip_states[nodes_to_check[length(nodes_to_check)],]) #Tip state
           both = c(nods, tps)
-          
+
           if(sum(both==current_state)==length(both)) { #If all states are equal to the state of the current node proceed.
             tips_in_segment = tips_in_segment+1
             tip_label_list = c(tip_label_list, names(tip_nodes[which(tip_nodes==nodes_to_check[length(nodes_to_check)])])) #Add the tips that will be removed.
@@ -242,7 +270,7 @@ count_import_export_ace_uncertainty = function(tree,ace_nodes,ace_tips, give_tip
         }
       }
     }
-    
+
     if(tips_in_segment > 1 ) {
       local_transmissions = local_transmissions+(tips_in_segment-1)
       importations = importations+1
@@ -251,7 +279,7 @@ count_import_export_ace_uncertainty = function(tree,ace_nodes,ace_tips, give_tip
       importations = importations+1
     }
     lineage_size = c(lineage_size,tips_in_segment)
-    
+
     lineage_tips[[counter]]=tip_label_list
     mrca.list[counter] = current_node
     counter = counter+1 #move one up to fill the next index on in the next loop iteration.
@@ -264,36 +292,7 @@ count_import_export_ace_uncertainty = function(tree,ace_nodes,ace_tips, give_tip
   if(start_time){
     lineage_mrca =lineage_mrca+start_time
   }
-  return(list("Import_Export"=c(importations, local_transmissions),"Lineage_sizes"=lineage_size,"Taxa_names"=lineage_tips, "MRCA's"=lineage_mrca))  
-  
+  return(list("Import_Export"=c(importations, local_transmissions),"Lineage_sizes"=lineage_size,"Taxa_names"=lineage_tips, "MRCA's"=lineage_mrca))
+
 }
 #count_import_export_ace(tree_test, ace_nodes=fit1$lik.anc,ace_tips = to.matrix(loc, seq=c("Norway", "RoW")), start_time=2000)
-#Test
-# Test = count_import_export_consensus(consensus_Q)
-# Test[1]; Test[2]
-# 
-# #Test on full tree
-# q2 = summary(mapped_cladeA)
-# 
-# #Find out why it doesn't work on the phylogeny - bug in the loop when it goes all the way back. Debug that aspect.
-# #DEBUG SECTION
-# q1 = summary(mapped_cladeA)
-# count_import_export_consensus(q1)[2]
-# #END OF DEBUG SECTION
-# 
-# 
-# q1 = summary(mapped_cladeA)
-# q2 = summary(mapped_cladeB)
-# q3 = summary(mapped_cladeC)
-# q4 = summary(mapped_cladeD)
-# a_lineages =count_import_export_consensus(q1, give_tips=tipsa)[[2]]
-# b_lineages =count_import_export_consensus(q2, give_tips=tipsb)[[2]]
-# c_lineages =count_import_export_consensus(q3, give_tips=tipsc)[[2]]
-# d_lineages =count_import_export_consensus(q4, give_tips=tipsd)[[2]]
-# all_lineages = c(a_lineages,b_lineages,c_lineages,d_lineages)
-# 
-# paste0("G:",1:length(all_lineages)," S: ", all_lineages)
-# atreemap = data.frame(group=paste0("G:",1:length(all_lineages)," S: ", all_lineages),Value=all_lineages)
-# pdf("~/Dropbox/10000_Lovers/Junk_used_for_exploratory/Lineage_sizes_norway_consensus_Ngon.pdf",width=10,height=10)
-# treemap(atreemap,index=("group"),vSize="Value", type="index", palette="Paired")
-# dev.off()
--- R/Relative_load_multi.R
+++ R/Relative_load_multi.R
@@ -3,9 +3,9 @@
 #E.G: If the group size is 2, then it is +0.5 importations, and +0.5 local transmissions.
 #E.G: IF the group size is 30 then it is +1/30 importation and + 29/30 local transmissions
 
+# library(data.table)
 
 #Load metadata Southafrican: Only for testing
-# library(data.table)
 # metadata = fread("~/Dropbox/Covid/Southafrican_03_24/nextstrain_groups_niph_ncov_southafrican-2021-04-07_metadata.tsv")
 # sum(tree$tip.label %in% metadata$Strain)
 # length(tree$tip.label)
@@ -14,7 +14,7 @@
 # names(dates)=metadata$Strain
 
 #Load metadata for the UK-variant.
-# library(data.table)
+
 # metadata = fread("~/Dropbox/Covid/UK_03_24/nextstrain_groups_niph_ncov_UK-centric-2021-04-07_metadata.tsv")
 # sum(tree$tip.label %in% metadata$Strain)
 # length(tree$tip.label)
@@ -24,20 +24,27 @@
 # names(dates)=metadata$Strain
 
 
-
-# @ Takes argument dates, should have names matching tip labels from tree.
-# @ Takes the phylogenetic tree used for ancestral state reconstruction.
-# @ Takes the Result_multi output from and replicated count script on castor (which runs on ace, simmap etc.)
-# @ Takes a list of taxa which to compute the importation load for: e.g. all tips in Norway 
-
-Relative_load_import_multi = function(tree,multicount, dates,start_time) {
+library(phytools)
+#' Title
+#'
+#' @param tree Takes the phylogenetic tree used for ancestral state reconstruction.
+#' @param multicount Takes the Result_multi output from and replicated count script on castor (which runs on ace, simmap etc.)
+#' @param dates Takes argument dates, should have names matching tip labels from tree.
+#' @param start_time Takes the date of the root of the phylogenetic tree.
+#' @param tip_list Takes a list of taxa which to compute the importation load for: e.g. all tips in Norway
+#'
+#' @return
+#' @export
+#' @importFrom phytools nodeHeights
+#' @examples
+Relative_load_import_multi = function(tree,multicount, dates,start_time,tip_list) {
   Result_multi = multicount
-  
+
   #Dates must have names that match the tip labels.
   LC = matrix(nrow=ncol(Result_multi),ncol=length(tip_list))
   Import = matrix(nrow=ncol(Result_multi), ncol=length(tip_list))
   date_matrix = matrix(nrow=ncol(Result_multi), ncol=length(tip_list))
-  
+
   print("Collecting import dates ")
   for(i in 1:ncol(Result_multi)) {
     print(i)/ncol(Result_multi)
@@ -53,31 +60,31 @@ Relative_load_import_multi = function(tree,multicount, dates,start_time) {
       date_matrix[i,j] = date_new
     }
   }
-  
+
   #Set windows for counting importations and local transmission in.
-  start_time 
+  start_time
   time_end = start_time+max(nodeHeights(tree))
   weeks = seq(start_time, time_end, by = 1/52)
   week_time=weeks[1:(length(weeks)-1)]+1/(52*2) #This is only for returning results at midpoint of weeks.
-  
+
   date_indexes = matrix(nrow=ncol(Result_multi), ncol=length(tip_list))
-  
+
   for(i in 1:ncol(Result_multi)) {
     date_indexes[i,] = sort.int(date_matrix[i,],decreasing = F,index.return = T)$ix
     Import[i,] = Import[i,date_indexes[i,]] #Sort imports after dates of observed samples.
     LC[i,] = LC[i,date_indexes[i,]] #Sort local transmissions after dates of observed samples.
   }
-  
+
   #Aggregate weekly importations
   t1 = table(cut(date_matrix[1,date_indexes[1,]], breaks=weeks))
   #Set up weekly matrices
   weekly_importations = matrix(nrow=ncol(Result_multi),ncol=length(t1))
   weekly_LC = matrix(nrow=ncol(Result_multi),ncol=length(t1))
-  
-  
+
+
   print("Aggregating weeks ")
   for(k in 1:ncol(Result_multi)){
-    
+
     print(k)/ncol(Result_multi)
     t1 = table(cut(date_matrix[k,date_indexes[k,]], breaks=weeks)) #Table that sorts observed tip dates
     j = 1
@@ -93,86 +100,14 @@ Relative_load_import_multi = function(tree,multicount, dates,start_time) {
       j = j+n
     }
   }
-  
-  
+
+
   list("Import"=weekly_importations,"LC"=weekly_LC, "Week_time"=week_time)
-  
-  
+
+
 }
 
 
-# 
+#
 # #Example of usage:
 # Result_with_uncertainty = Relative_load_import_multi(tree, multi_counts,dates, start_date)
-#  
-# start_time = start_date
-# #Set windows for counting importations and local transmission in.
-# start_time 
-# time_end = start_time+max(nodeHeights(tree))
-# weeks = seq(start_time, time_end, by = 1/52)
-# week_time=weeks[1:(length(weeks)-1)]+1/(52*2) #This is only for returning results at midpoint of weeks.
-# weekly_importations = apply(Result_with_uncertainty$Import,2, FUN="mean")
-# weekly_local_tranmissions = apply(Result_with_uncertainty$LC,2, FUN="mean")
-# sum_cases = weekly_importations+weekly_local_tranmissions
-# 
-# 
-# #This uses the mean and discards uncertainty.
-# ggdata = data.frame(dates = as.Date(date_decimal(week_time)),
-#                   fraction=c(weekly_importations/sum_cases,weekly_local_tranmissions/sum_cases),
-#                   group=c(rep("Import",length(week_time)),
-#                           rep("Local transmission",length(week_time))))
-# 
-# pdf(paste0(path_to_results,"relative_load.pdf"), width=15,height=7.5)
-# g1 = ggplot(data=ggdata, aes(x=dates, y=fraction, fill=group)) + 
-#   geom_area(alpha=0.6 , size=1, colour="black")+
-#   theme_minimal(base_size=20)+scale_fill_brewer(palette="Set1")+
-#   scale_x_date(date_breaks="1 week")+
-#   theme(axis.text.x=element_text(angle=60, hjust=1))+
-#   ylab("")+xlab("")+
-#   annotate("rect",xmin=as.Date(decimal2Date(time_end-1/12)),xmax=as.Date(dat$dates[length(dat$dates)]),ymin=-Inf,ymax=Inf,alpha=0.2)+
-#   theme(legend.position = c(0.55,0.15), legend.title=element_blank())
-# g1
-# dev.off()
-# 
-# #Plotting method
-# imports = t(apply(Result_with_uncertainty$Import, 2,quantile,c(0.025,0.5,0.975)))
-# exports = t(apply(Result_with_uncertainty$LC, 2,quantile,c(0.025,0.5,0.975)))
-# dat=data.frame(cbind(imports,exports),dates=decimal2Date(week_time))
-# colnames(dat)=c("i2.5","i50","i97.5","e2.5","e50","e97.5","dates")
-# 
-# 
-# #Line for UK
-# #scale_x_date(date_breaks="1 week",limits =c(as.Date("2020-11-30"), as.Date("2021-03-15")))+ # limits=c(as.Date("2020-06-30"), as.Date("2021-03-29"))
-# 
-# svglite(paste0(path_to_results,"Imports.svg"), width=10,height=5)
-# g1 = ggplot(dat, aes(x=dates,y=i50))+geom_line(col="Red")+theme_minimal(base_size=20)+
-#   theme(axis.title.x = element_blank(), axis.text.x = element_blank())+
-#   ylim(0,5)+
-#   geom_ribbon(aes(ymin=i2.5, ymax=i97.5,alpha=0.2), fill="lightblue")+
-#   scale_x_date(date_breaks="1 week",limits =c(as.Date("2020-11-30"), as.Date("2021-03-28")),expand=c(0,0))+ # limits=c(as.Date("2020-06-30"), as.Date("2021-03-29"))
-#   #theme(axis.text.x=element_text(angle=60, hjust=1))+scale_fill_continuous("Grey")+xlab("")+
-#   ylab("Import")+
-#   theme(legend.position = "none")+annotate("rect",xmin=as.Date(decimal2Date(time_end-1/12)),xmax=as.Date(dat$dates[length(dat$dates)]),ymin=-Inf,ymax=Inf,alpha=0.2)
-# g1
-# dev.off()
-# 
-# 
-# #Line for UK
-# #scale_x_date(date_breaks="1 week",limits =c(as.Date("2020-11-30"), as.Date("2021-03-15")))+ #limits =c(as.Date("2021-01-18"), as.Date("2021-03-15"))
-# 
-# svglite(paste0(path_to_results,"Local_transmissions.svg"), width=10,height=5)
-# g2 = ggplot(dat, aes(x=dates,y=e50))+geom_line(col="Darkgreen")+theme_minimal(base_size=20)+ylim(0,45)+
-#   scale_x_date(date_breaks="1 week",limits =c(as.Date("2020-11-30"), as.Date("2021-03-28")), expand=c(0,0))+ #limits =c(as.Date("2021-01-18"), as.Date("2021-03-15"))
-#   geom_ribbon(aes(ymin=e2.5, ymax=e97.5,alpha=0.2), fill="lightblue")+
-#   theme(axis.text.x=element_text(angle=60, hjust=1))+scale_fill_continuous("Grey")+ylab("Local transmission")+xlab("")+
-#   theme(legend.position = "none")+annotate("rect",xmin=as.Date(decimal2Date(time_end-1/12)),xmax=as.Date(dat$dates[length(dat$dates)]),ymin=-Inf,ymax=Inf,alpha=0.2)
-# g2
-# dev.off()
-# 
-# 
-# graphics.off()
-# pdf(paste0(path_to_results,"Import_export.pdf"),width=7.5,height=7.5)
-# #grid.arrange(grobs=list(g1,g2),layout_matrix=rbind(c(1,1),c(2,2)))
-# #grid.arrange(grobs=list(g1,g2),ncol=1)
-# grid.draw(rbind(ggplotGrob(g1), ggplotGrob(g2), size = "last"))
-# dev.off()
